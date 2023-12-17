c7e44c4249d42889aa9b8d62e77ceb8bba9f2e4d
Danilo Chang <ray2501@gmail.com>
2023-12-17 16:59:50 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded jsonnet 0.15 \
-    [list load [file join $dir libjsonnet0.15.so] jsonnet]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded jsonnet 0.15 \
+	    [list load [file join $dir libtcl9jsonnet0.15.so] [string totitle jsonnet]]
+} else {
+    package ifneeded jsonnet 0.15 \
+	    [list load [file join $dir libjsonnet0.15.so] [string totitle jsonnet]]
+}
